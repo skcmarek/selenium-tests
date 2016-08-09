d9eb90be941c991a00ebd5a8d219626a00429ee4
Bogna Knychala <bogna.ka@gmail.com>
2016-08-09 16:58:00 +0200

    ADEN-3753 | create script to gather metrics for ads recovery

---
+++ src/test/java/com/wikia/webdriver/testcases/adstests/TestAdsRecoveryMetricsOasis.java
@@ -0,0 +1,62 @@
+package com.wikia.webdriver.testcases.adstests;
+
+import com.google.common.collect.ImmutableMap;
+import com.wikia.webdriver.common.contentpatterns.AdsContent;
+import com.wikia.webdriver.common.core.url.Page;
+import com.wikia.webdriver.common.templates.NewTestTemplate;
+import com.wikia.webdriver.pageobjectsfactory.pageobject.adsbase.AdsRecoveryObject;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.WebElement;
+import org.testng.annotations.Test;
+
+import java.util.Map;
+
+public class TestAdsRecoveryMetricsOasis extends NewTestTemplate {
+
+  private static Dimension DESKTOP_SIZE = new Dimension(1920, 1080);
+
+  @Test(
+          groups = "AdsRecoveryMetricsOasis",
+          invocationCount = 5
+  )
+  public void adsRecoveryMetricsOasis() {
+    Page page = new Page("arecovery", "SyntheticTests/Static_image");
+    String url = urlBuilder.getUrlForPage(page);
+    AdsRecoveryObject adsBaseObject = new AdsRecoveryObject(driver, url, DESKTOP_SIZE);
+
+    //TOP_LEADERBOARD
+    Map<String, Object> leaderBoardSlot = ImmutableMap.<String, Object>builder()
+            .put("adUnitId", "wikia_gpt/5441/wka.life/_arecovery//article/gpt/TOP_LEADERBOARD")
+            .put("slotName", AdsContent.TOP_LB)
+            .put("lineItemId", 277592292)
+            .put("src", "gpt")
+            .build();
+    String adUnitIdLeaderboard = leaderBoardSlot.get("adUnitId").toString();
+    String slotNameLeaderboard = leaderBoardSlot.get("slotName").toString();
+
+    String recoveredAdUnitIdSelectorLeaderboard = "#" + adsBaseObject.getRecoveredAdUnitId(adUnitIdLeaderboard);
+    WebElement recoveredSlotLeaderboard = driver.findElement(By.cssSelector(recoveredAdUnitIdSelectorLeaderboard));
+
+    adsBaseObject.triggerAdSlot(slotNameLeaderboard)
+            .verifyLineItemId(slotNameLeaderboard, Integer.valueOf(leaderBoardSlot.get("lineItemId").toString()))
+            .verifyExpandedAdVisibleInSlot(recoveredAdUnitIdSelectorLeaderboard, recoveredSlotLeaderboard);
+
+    //MEDREC
+    Map<String, Object> slotInfo = ImmutableMap.<String, Object>builder()
+            .put("adUnitId", "wikia_gpt/5441/wka.life/_arecovery//article/gpt/TOP_RIGHT_BOXAD")
+            .put("slotName", AdsContent.MEDREC)
+            .put("lineItemId", 277592292)
+            .put("src", "gpt")
+            .build();
+    String adUnitId = slotInfo.get("adUnitId").toString();
+    String slotName = slotInfo.get("slotName").toString();
+
+    String recoveredAdUnitIdSelector = "#" + adsBaseObject.getRecoveredAdUnitId(adUnitId);
+    WebElement recoveredSlot = driver.findElement(By.cssSelector(recoveredAdUnitIdSelector));
+
+    adsBaseObject.triggerAdSlot(slotName)
+            .verifyLineItemId(slotName, Integer.valueOf(slotInfo.get("lineItemId").toString()))
+            .verifyExpandedAdVisibleInSlot(recoveredAdUnitIdSelector, recoveredSlot);
+  }
+}
